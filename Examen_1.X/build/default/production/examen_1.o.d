a6d213f7416914c096590e75edfeeec69837bf7a
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2025-10-02 01:08:32 -0600

    Agregar examen y el proyecto bonito de 6 LEDS

---
+++ Examen_1.X/build/default/production/examen_1.o.d
@@ -0,0 +1,3 @@
+build/default/production/examen_1.o:  \
+	C:/Users/jdqg2/MPLABXProjects/Examen_1.X/examen_1.asm  \
+
---
+++ Proyecto_6_LEDS.X/build/default/production/6_LEDS_bonito.o.d
@@ -0,0 +1,3 @@
+build/default/production/6_LEDS_bonito.o:  \
+	C:/Users/jdqg2/MPLABXProjects/Proyecto_6_LEDS.X/6_LEDS_bonito.asm  \
+
